7fe257562107268d85296d54ffa6bda3b04ca126
aksadroit <AKSHAT4@GMAIL.COM>
2021-04-18 14:21:50 +0530

    Refactored: Extracted string literals as constant and renamed the method parameters in business logic

--- src/main/java/org/tictactoe/tdd/demo/TicTacToeGame.java
+++ src/main/java/org/tictactoe/tdd/demo/TicTacToeGame.java
@@ -2,7 +2,14 @@ package org.tictactoe.tdd.demo;
 
 public class TicTacToeGame {
 
-	private char[][] gameBoard = new char[3][3];
+	private static final int GAME_BOARD_SIZE = 3;
+	private static final char MOVE_X = 'X';
+	private static final String PLAYER_X = "Player_X";
+	private static final String PLAYER_Y = "Player_Y";
+	
+	
+	private char[][] gameBoard = new char[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
+	
 	
 	public void makeMoveAt(int row, int column, char playerMove) {
 		gameBoard[row][column] = playerMove;
@@ -12,13 +19,13 @@ public class TicTacToeGame {
 		return gameBoard[row][column];
 	}
 
-	public String identifyPlayerForGivenLocation(int i, int j) {
+	public String identifyPlayerForGivenLocation(int row, int column) {
 		String player;
 		
-		if (identifyMoveAt(i, j) == 'X')
-			player = "Player_X";
+		if (identifyMoveAt(row, column) == MOVE_X)
+			player = PLAYER_X;
 		else {
-			player = "Player_O";
+			player = PLAYER_Y;
 		}
 		return player;
 	}
