718f7c0c946d883814941d87a75a796b445ea83f
aksadroit <AKSHAT4@GMAIL.COM>
2021-04-18 14:41:35 +0530

    Refactored: Extracted constant string literals for test cases and added initialise block

--- src/test/java/org/tictactoe/tdd/demo/TicTacToeGameTest.java
+++ src/test/java/org/tictactoe/tdd/demo/TicTacToeGameTest.java
@@ -2,22 +2,32 @@ package org.tictactoe.tdd.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TicTacToeGameTest {
+	
+	private static final int POS_1 = 1;
+	private static final char MOVE_X = 'X';
+	private static final String PLAYER_X = "Player_X";
+	
+	private TicTacToeGame game;
+	
+	@BeforeEach
+	public void init() {
+		game = new TicTacToeGame();
+	}
 
 	@Test
 	public void playerOneShouldBeAbleToMakeMoveInAnyPositionAndIdentifyTheSame() {
-		TicTacToeGame game = new TicTacToeGame();
-		game.makeMoveAt(1, 1, 'X');
-		assertEquals('X', game.identifyMoveAt(1, 1));
+		game.makeMoveAt(POS_1, POS_1, MOVE_X);
+		assertEquals(MOVE_X, game.identifyMoveAt(POS_1, POS_1));
 	}
 	
 	@Test
 	public void indentifyPlayerByObservingMoveAtAnyGivenLocation() {
-		TicTacToeGame game = new TicTacToeGame();
-		game.makeMoveAt(1, 1, 'X');
-		assertEquals("Player_X", game.identifyPlayerForGivenLocation(1, 1));
+		game.makeMoveAt(POS_1, POS_1, MOVE_X);
+		assertEquals(PLAYER_X, game.identifyPlayerForGivenLocation(POS_1, POS_1));
 	}
 
 }
